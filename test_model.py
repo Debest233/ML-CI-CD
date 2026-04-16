e9bade0518c5d55b364c8fcd05a54f87a28b623a
Mateusz <debest233.kermitpl@gmail.com>
2026-04-16 10:16:22 +0200

    Wersja końcowa

--- test_model.py
+++ test_model.py
@@ -7,13 +7,13 @@ def test_predictions_not_none():
     assert preds is not None, "Predictions should not be None."
 
 def test_predictions_length():
-    """Test 2 (na 5): Sprawdza, czy długość listy predykcji jest > 0 i odpowiada próbkom testowym."""
+    """Test 2: Sprawdza, czy długość listy predykcji jest > 0 i odpowiada próbkom testowym."""
     preds, y_test = train_and_predict()
     assert len(preds) > 0, "Długość predykcji powinna być większa od zera."
     assert len(preds) == len(y_test), "Liczba predykcji musi odpowiadać liczbie testowych etykiet."
 
 def test_predictions_value_range():
-    """Test 3 (na 5): Sprawdza, czy wartości mieszczą się w zakresie dla zbioru Iris (0, 1, 2)."""
+    """Test 3: Sprawdza, czy wartości mieszczą się w zakresie dla zbioru Iris (0, 1, 2)."""
     preds, _ = train_and_predict()
     # Sprawdzamy, czy każda przewidziana klasa znajduje się w dopuszczalnym zbiorze
     assert all(p in [0, 1, 2] for p in preds), "Predykcje zawierają nieoczekiwane klasy!"
